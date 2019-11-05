6a127a5e291ee56f66739a9951e912e9cf03fab8
Alex <vokdub13@gmail.com>
2019-11-06 00:23:52 +0300

    single style coding

--- pages/card/index.vue
+++ pages/card/index.vue
@@ -2,58 +2,58 @@
 	<div>
 		
 		<nuxt-link to="/">main</nuxt-link>
-			
-			<h1>Название: {{description.Title}}</h1>
-			<p>Описание: {{description.Description}}</p>
-			<p>Цена: {{description.Price}}</p>
+
+		<h1>Название: {{description.Title}}</h1>
+		<p>Описание: {{description.Description}}</p>
+		<p>Цена: {{description.Price}}</p>
 		
-	<button @click="greet">купить</button>
+		<button @click="greet">купить</button>
 	</div>
 </template>
 
 <script>
 	export default{
 		head: {
-    		title: 'Описание'
-    	},
+			title: 'Описание'
+		},
 		data() {
 			return{
 				description: 
-        			{
-                "id": 1,
-                "Title": "Попка корги",
-                "Description": "Туда можно засунуть ценные вещички :З",
-                "Price": 2000.0,
-                "photos": [
-                    {
-                        "Direction": "dsfgfgsdfgsf",
-                        "alt": "picture"
-                    },
-                    {
-                        "Direction": "В зеленолупье",
-                        "alt": "picture"
-                    }
-                ],
-                "modifications": [
-                    {
-                        "color": "red",
-                        "size": "S"
-                    },
-                    {
-                        "color": "orange",
-                        "size": "L"
-                    },
-                    {
-                        "color": "green",
-                        "size": "S"
-                    }
-                ]
-            }
-		        
+				{
+					"id": 1,
+					"Title": "Попка корги",
+					"Description": "Туда можно засунуть ценные вещички :З",
+					"Price": 2000.0,
+					"photos": [
+					{
+						"Direction": "dsfgfgsdfgsf",
+						"alt": "picture"
+					},
+					{
+						"Direction": "В зеленолупье",
+						"alt": "picture"
 					}
+					],
+					"modifications": [
+					{
+						"color": "red",
+						"size": "S"
+					},
+					{
+						"color": "orange",
+						"size": "L"
+					},
+					{
+						"color": "green",
+						"size": "S"
+					}
+					]
+				}
+
+			}
 		},
-		 methods: {
-    greet: function (event) {
+		methods: {
+			greet (event) {
       // `this` внутри методов указывает на экземпляр Vue
       alert('вы приобрели товар, все гуд. Куплен товар '+ this.description.Title)
       
@@ -61,17 +61,17 @@
   }
   ,
   // REGEN
-		created: function() {
-			this.$store.dispatch('getDescription');
-		},
-		computed: {
-			goods () {
-				return this.$store.state.description;
-			}
-		}
-	}
+  created () {
+  	this.$store.dispatch('getDescription');
+  },
+  computed: {
+  	goods () {
+  		return this.$store.state.description;
+  	}
+  }
+}
 </script> 
 
 <style>
-	
+
 </style>
